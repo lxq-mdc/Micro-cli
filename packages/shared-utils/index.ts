b39794eed968fff3174f3b16bba517f4ae99d9a8
MYY1106 <361598804@qq.com>
2022-10-11 22:40:32 +0800

    fix: fix bug in importing module

--- packages/shared-utils/index.ts
+++ packages/shared-utils/index.ts
@@ -1,20 +1,7 @@
-// import chalk from 'chalk';
-// import chalk from 'chalk';
-// import semver from 'semver';
-// import log from './lib/log';
-
-// export default {
-//     log ,
-//     chalk,
-//     semver
-// };
-
-// eslint-disable-next-line import/prefer-default-export
-import exit from './lib/exit';
-import { compile, writeToFile } from './lib/compile';
-import extendPackage from './lib/extendPackage';
-
-export { exit, compile, writeToFile, extendPackage };
+export { default as exit } from './lib/exit';
+export { compile, writeToFile } from './lib/compile';
+export { default as extendPackage } from './lib/extendPackage';
+// export { exit, compile, writeToFile, extendPackage };
 export { default as chalk } from 'chalk';
 export { default as semver } from 'semver';
 export { isRoot, isTypescript } from './lib/is';
